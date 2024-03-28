64fbc287dc14378fa79338d5be553f8113a119a5
azizHr <azizharkati20@gmail.com>
2024-03-28 13:36:55 +0000

    delete user.actions

--- src/app/store/actions/user.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import {ManagerResponse} from "../../models/response/manager-response";
-import {StudentResponse} from "../../models/response/student-response";
-import {TeacherResponse} from "../../models/response/teacher-response";
-
-export const getLoggedInUser = createAction("[User] Get Logged In User");
-export const getLoggedInUserSuccess = createAction("[User] Get Logged In User Success", props<{ user: ManagerResponse | StudentResponse | TeacherResponse | null }>());
-export const getLoggedInUserFailure = createAction("[User] Get Logged In User Failure", props<{ error: string }>());
