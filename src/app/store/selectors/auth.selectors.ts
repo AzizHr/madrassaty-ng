65d51abc67f08d2431e8550a2838c45ca2064126
azizHr <azizharkati20@gmail.com>
2024-03-28 13:29:40 +0000

    update user.selectors name to auth.selectors

--- src/app/store/selectors/auth.selectors.ts
+++ src/app/store/selectors/auth.selectors.ts
@@ -1,8 +1,8 @@
 import { createSelector } from '@ngrx/store';
 import {AppState} from "../../state/app.state";
 
-export const selectFeature = (state: AppState) => state.user;
+export const selectFeature = (state: AppState) => state.auth;
 
 export const isLoadingSelector = createSelector(selectFeature, (state) => state.isLoading);
-export const userSelector = createSelector(selectFeature, (state) => state.user);
+export const authSelector = createSelector(selectFeature, (state) => state.auth);
 export const errorSelector = createSelector(selectFeature, (state) => state.error);
