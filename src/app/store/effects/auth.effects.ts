e01b79aeb60a7974c71141e55c6f8823738ab45f
azizHr <azizharkati20@gmail.com>
2024-03-28 13:46:54 +0000

    add effects to auth.effects file

--- src/app/store/effects/auth.effects.ts
+++ src/app/store/effects/auth.effects.ts
@@ -2,22 +2,48 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { catchError, map, mergeMap } from 'rxjs/operators';
-import * as AuthActions from '../actions/school.actions';
+import * as AuthActions from '../actions/auth.actions';
 import {Router} from "@angular/router";
 import Swal from "sweetalert2";
 import {ManagerAuthService} from "../../services/auth/manager/manager-auth.service";
+import {StudentAuthService} from "../../services/auth/student/student-auth.service";
+import {TeacherAuthService} from "../../services/auth/teacher/teacher-auth.service";
 
 @Injectable()
 export class SchoolEffects {
-  constructor(private actions$: Actions, private managerAuthService: ManagerAuthService, private router: Router) {}
+  constructor(private actions$: Actions, private managerAuthService: ManagerAuthService, private studentAuthService: StudentAuthService, private teacherAuthService: TeacherAuthService, private router: Router) {}
 
   getLoggedInManager$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(SchoolActions.loadSchoolByManagerId),
-      mergeMap((action) =>
-        this.schoolService.getByManagerId(action.managerId).pipe(
-          map((schoolResponse) => SchoolActions.loadSchoolByManagerIdSuccess({ schoolResponse })),
-          catchError((error) => of(SchoolActions.loadSchoolByManagerIdFailure({ error: error.message })))
+      ofType(AuthActions.getLoggedInManager),
+      mergeMap(() =>
+        this.managerAuthService.getLoggedInManager().pipe(
+          map((auth) => AuthActions.getLoggedInManagerSuccess({ auth })),
+          catchError((error) => of(AuthActions.getLoggedInManagerFailure({ error: error.message })))
+        )
+      )
+    )
+  );
+
+  getLoggedInStudent$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.getLoggedInStudent),
+      mergeMap(() =>
+        this.studentAuthService.getLoggedInStudent().pipe(
+          map((auth) => AuthActions.getLoggedInStudentSuccess({ auth })),
+          catchError((error) => of(AuthActions.getLoggedInStudentFailure({ error: error.message })))
+        )
+      )
+    )
+  );
+
+  getLoggedInTeacher$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.getLoggedInTeacher),
+      mergeMap(() =>
+        this.teacherAuthService.getLoggedInTeacher().pipe(
+          map((auth) => AuthActions.getLoggedInTeacherSuccess({ auth })),
+          catchError((error) => of(AuthActions.getLoggedInTeacherFailure({ error: error.message })))
         )
       )
     )
