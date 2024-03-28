17a0f12b6c18a0f274e1b902de3b5609d8427632
azizHr <azizharkati20@gmail.com>
2024-03-28 13:28:23 +0000

    add actions to manager.actions file

---
+++ src/app/store/actions/manager.actions.ts
@@ -0,0 +1,6 @@
+import {createAction, props} from "@ngrx/store";
+import {ManagerResponse} from "../../models/response/manager-response";
+
+export const getLoggedInManager = createAction("[Manager] Get Logged In Manager");
+export const getLoggedInManagerSuccess = createAction("[Manager] Get Logged In Manager Success", props<{ user: ManagerResponse }>());
+export const getLoggedInManagerFailure = createAction("[Manager] Get Logged In Manager Failure", props<{ error: string }>());
