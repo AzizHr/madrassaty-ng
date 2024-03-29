4bc00cfe7b7081c5c9ec7c2129f629e6d97b1f7d
azizHr <azizharkati20@gmail.com>
2024-03-29 10:20:32 +0000

    get logged in teacher

--- src/app/components/teacher/teacher-dashboard/teacher-dashboard.component.ts
+++ src/app/components/teacher/teacher-dashboard/teacher-dashboard.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {ManagerProfileResponse} from "../../../models/response/manager-profile-response";
+import {StudentProfileResponse} from "../../../models/response/student-profile-response";
+import {TeacherProfileResponse} from "../../../models/response/teacher-profile-response";
+import {select, Store} from "@ngrx/store";
+import {AppState} from "../../../state/app.state";
+import {authSelector} from "../../../store/selectors/auth.selectors";
+import * as AuthActions from "../../../store/actions/auth.actions";
 
 @Component({
   selector: 'app-teacher-dashboard',
   templateUrl: './teacher-dashboard.component.html',
   styleUrls: ['./teacher-dashboard.component.css']
 })
-export class TeacherDashboardComponent {
+export class TeacherDashboardComponent implements OnInit {
+
+  auth$: Observable<ManagerProfileResponse | StudentProfileResponse | TeacherProfileResponse>
+  auth: ManagerProfileResponse | StudentProfileResponse | TeacherProfileResponse;
+
+  public constructor(private store: Store<AppState>) {
+    this.auth$ = this.store.pipe(select(authSelector))
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(AuthActions.getLoggedInTeacher());
+    this.auth$.subscribe(auth => this.auth = auth);
+  }
 
 }
